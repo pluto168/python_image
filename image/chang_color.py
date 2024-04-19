6743a9d9cc05a349b5fc2c766217cd27de7ad451
pluto168 <86468019+pluto168@users.noreply.github.com>
2024-04-19 21:12:26 +0800

    fix text content.

--- image/chang_color.py
+++ image/chang_color.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 def pickAFile():
     root = Tk()
-    root.withdraw()  # 隐藏根窗口
+    root.withdraw()  
     file_path = filedialog.askopenfilename()  # 打开文件对话框并获取选择的文件路径
-    root.destroy()  # 关闭根窗口
+    root.destroy()  
     return file_path
 
 def getPixel(image, x, y):
@@ -15,7 +15,7 @@ def setColor(image, x, y, color):
     image.putpixel((x, y), color)
 
 def distance(color1, color2):
-    # 计算两个颜色之间的欧几里得距离
+    # 计算兩個顏色之間的歐幾裡得距離
     return sum((component1 - component2) ** 2 for component1, component2 in zip(color1, color2)) ** 0.5
 
 def changeWhiteToBlue(image):
@@ -29,10 +29,9 @@ def changeWhiteToBlue(image):
     return image
 
 if __name__ == "__main__":
-    filepath = pickAFile()  # 用户选择文件
+    filepath = pickAFile()  
     if filepath:
-        picture = Image.open(filepath)  # 打开图片
-        modified_picture = changeWhiteToBlue(picture)  # 修改指定颜色
+        picture = Image.open(filepath)  
+        modified_picture = changeWhiteToBlue(picture)  # 修改指定顏色
         
-        # 保存修改后的图片
-        modified_picture.show()  # 显示修改后的图片
+        modified_picture.show()  
